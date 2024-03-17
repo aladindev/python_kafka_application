a1b7e7441ea4e424f64e54b8eb50f80a999d58cc
aladindev <kjune0205@naver.com>
2024-03-17 15:34:14 +0900

    Python Kafka Application migration

---
+++ upbit_api.py
@@ -0,0 +1,26 @@
+import os
+import jwt
+import uuid
+import hashlib
+from urllib.parse import urlencode
+
+import requests
+
+# 환경 변수에서 Access Key와 Secret Key를 불러옵니다.
+access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
+secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
+server_url = os.environ['UPBIT_SERVER_URL']
+
+payload = {
+    'access_key': access_key,
+    'nonce': str(uuid.uuid4()),
+}
+
+jwt_token = jwt.encode(payload, secret_key)
+authorize_token = 'Bearer {}'.format(jwt_token)
+headers = {"Authorization": authorize_token}
+
+res = requests.get(server_url + "/v1/accounts", headers=headers)
+
+print(res.json())
+
