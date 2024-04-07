54412dfa3bcb61dbe226e3a8dd41e7dcc42f9bbd
aladindev <kjune0205@naver.com>
2024-04-07 15:23:03 +0900

    Ollama Model

--- apps/ollama/main.py
+++ apps/ollama/main.py
@@ -2,8 +2,10 @@
 log.info(f"form_data.model_dump_json(exclude_none=True) > {form_data.model_dump_json(exclude_none=True)}")
 
 log.info(f"form_data> > > {form_data}")
+# form_data 객체에서 딕셔너리 데이터를 가져옴
+form_dict = form_data.to_dict()
 
-user_messages = [msg for msg in form_data['messages'] if msg['role'] == 'user']
+user_messages = [msg for msg in form_dict['messages'] if msg['role'] == 'user']
 if user_messages:  # user_messages 리스트가 비어있지 않은 경우에만 실행
     last_user_content = user_messages[-1]['content']
     log.info(f"last_user_content >> {last_user_content}")
