902b43a8b50d14c85253d7a9ad989585e702dd5f
aladindev <kjune0205@naver.com>
2024-04-07 15:11:49 +0900

    Ollama Model

--- apps/ollama/main.py
+++ apps/ollama/main.py
@@ -3,5 +3,5 @@ from langchain.vectorstores import Chroma
 log.info(f"form_data> > > {form_data}")
 #db3 = Chroma(persist_directory="./chroma_db", embedding_function=form_data.model_dump_json(exclude_none=True).model)
 db3 = Chroma(persist_directory="./chroma_db", embedding_function=form_data.model)
-docs = db3.similarity_search(query)
+docs = db3.similarity_search(form_data.content)
 log.info(f"docs >>>  {docs}")
