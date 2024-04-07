9a453d16e6c89af9620bbc6e15ab77fab2aac9dc
aladindev <kjune0205@naver.com>
2024-04-07 15:19:58 +0900

    Ollama Model

--- apps/ollama/main.py
+++ apps/ollama/main.py
@@ -1,7 +1,18 @@
 
-from langchain.vectorstores import Chroma
+log.info(f"form_data.model_dump_json(exclude_none=True) > {form_data.model_dump_json(exclude_none=True)}")
+
 log.info(f"form_data> > > {form_data}")
-#db3 = Chroma(persist_directory="./chroma_db", embedding_function=form_data.model_dump_json(exclude_none=True).model)
+
+user_messages = [msg for msg in form_data['messages'] if msg['role'] == 'user']
+if user_messages:  # user_messages 리스트가 비어있지 않은 경우에만 실행
+    last_user_content = user_messages[-1]['content']
+    log.info(f"last_user_content >> {last_user_content}")
+else:
+    log.info("No user messages found")
+
+
+from langchain.vectorstores import Chroma
 db3 = Chroma(persist_directory="./chroma_db", embedding_function=form_data.model)
-docs = db3.similarity_search(form_data.content)
+docs = db3.similarity_search(last_user_content)
 log.info(f"docs >>>  {docs}")
+
