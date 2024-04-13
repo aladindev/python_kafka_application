61df2914fb1032864fbed4d2b95dc41dd1325a57
aladindev <kjune0205@naver.com>
2024-04-13 15:16:45 +0900

    ChromaDB collection / find min_distance

--- apps/ollama/main.py
+++ apps/ollama/main.py
@@ -110,3 +110,89 @@ db3 = Chroma(persist_directory="./chroma_db", embedding_function=form_data.model
 docs = db3.similarity_search(last_user_content)
 log.info(f"docs >>>  {docs}")
 
+
+
+
+
+#0413
+#ollama/main.py
+            #log.info(f"form_data.model_dump_json(exclude_none=True) > {form_data.model_dump_json(exclude_none=True)}")
+
+            #log.info(f"form_data> > > {form_data}")
+            # form_data 객체에서 딕셔너리 데이터를 가져옴
+            form_dict = form_data.dict()
+
+            user_messages = [msg for msg in form_dict['messages'] if msg['role'] == 'user']
+            if user_messages:  # user_messages 리스트가 비어있지 않은 경우에만 실행
+                last_user_content = user_messages[-1]['content']
+                log.info(f"last_user_content >> {last_user_content}")
+            else:
+                log.info("No user messages found")
+
+            #0410 check1
+            #client = chromadb.PersistentClient(path="../../data/uploads")    
+            # collection = client.get_or_create_collection(
+            #     name="All-Documents"
+            # )
+            #     # DB 쿼리
+            # collection.query(
+            #     query_texts=["user_messages"],
+            #     n_results=5,
+            # )
+            #0410
+            #../../data/vector_db'
+            #log.info("1")
+            #embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+            #persist_directory = '/Users/kimjunhyeok/Desktop/Visual Studio Code/open-webui/open-webui/backend/data/test_vector_db'
+            #persist_directory = '/Users/kimjunhyeok/Desktop/Visual Studio Code/open-webui/open-webui/backend/data/vector_db'
+            #vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding_function)
+            #vectordb = chromadb.PersistentClient(path=persist_directory)
+
+            #0413
+            chromadb = CHROMA_CLIENT
+            collections = chromadb.list_collections()
+            # 최소 거리의 초기값을 무한대로 설정합니다.
+            min_distance = float('inf')
+            # 최소 거리에 해당하는 결과를 저장할 변수를 초기화합니다.
+            min_result = None
+            # 최소 거리를 가진 컬렉션의 이름을 저장할 변수를 초기화합니다.
+            min_collection_name = ''
+
+            for collection in collections:
+                collection_name = collection.name
+    
+                # 해당 이름을 사용하여 컬렉션 인스턴스를 가져옵니다.
+                collection_instance = chromadb.get_collection(collection_name)
+                result = collection_instance.query(
+                    query_texts=[user_messages],
+                    n_results=5,
+                )
+
+                log.info(f"Results for collection {collection_name}: {result}")
+                # 결과에서 distances의 값이 가장 작은지 확인합니다.
+                if result['distances'] and result['distances'][0]:
+                    if result['distances'][0][0] < min_distance:
+                        # 현재 결과의 거리가 더 작다면, 최소값을 업데이트합니다.
+                        min_distance = result['distances'][0][0]
+                        min_result = result
+                        min_collection_name = collection_name
+                    log.info(f"Results for collection {collection_name}: {result}")
+    
+            log.info("######################## MIN DISTANCE ############################# \n\n\n\n\n")
+            collection_instance = chromadb.get_collection(min_collection_name)
+            result = collection_instance.query(
+                    query_texts=[user_messages],
+                    n_results=5,
+            )
+            log.info(f"Results for collection {collection_name}: {result}")
+            log.info("######################## MIN DISTANCE ############################# \n\n\n\n\n")
+
+            # DB 쿼리
+            #retriever = chromadb.as_retriever()
+            #result = retriever.invoke("일산")
+            #docs = retriever.get_relevant_documents("일산")
+            # for doc in docs:
+            #     log.info(doc.metadata["source"])
+            # embedding_vector = embedding_function.embed_query("일산")
+            # docs = vectordb.similarity_search_by_vector(embedding_vector)
+            #log.info(f"docs >>>>>>> {docs}")
