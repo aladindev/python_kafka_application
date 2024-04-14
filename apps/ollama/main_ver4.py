c27477e900999c043fbfa1aebade58c0e3e749c4
aladindev <kjune0205@naver.com>
2024-04-14 17:55:40 +0900

    Chroma VectorDB Embedding insert / RAG Search

--- apps/ollama/main_ver4.py
+++ apps/ollama/main_ver4.py
@@ -665,8 +665,6 @@ async def generate_completion(
     user=Depends(get_current_user),
 ):
 
-
-    log.info(f"form_data >> {form_data}")
     if url_idx == None:
         if form_data.model in app.state.MODELS:
             url_idx = random.choice(app.state.MODELS[form_data.model]["urls"])
@@ -705,30 +703,6 @@ async def generate_completion(
                         r.close()
                         if request_id in REQUEST_POOL:
                             REQUEST_POOL.remove(request_id)
-
-                # form_data 객체에서 딕셔너리 데이터를 가져옴
-                # form_dict = form_data.dict()
-
-                # log.info(f"form_dict >> {form_dict}")
-
-                # user_messages = [msg for msg in form_dict['messages'] if msg['role'] == 'user']
-                # if user_messages:  # user_messages 리스트가 비어있지 않은 경우에만 실행
-                #     last_user_content = user_messages[-1]['content']
-                #     log.info(f"last_user_content >> {last_user_content}")
-                # else:
-                #     log.info("No user messages found")
-
-                # model_name = "jhgan/ko-sroberta-multitask" # (KorNLU 데이터셋에 학습시킨 한국어 임베딩 모델)
-                # model_kwargs = {'device': 'cpu'}
-                # encode_kwargs = {'normalize_embeddings': False}
-                # embedding_model = HuggingFaceEmbeddings(
-                #     model_name=model_name,
-                #     model_kwargs=model_kwargs,
-                #     encode_kwargs=encode_kwargs
-                # )
-                # db3 = Chroma(persist_directory="./chroma_db", embedding_function=form_data.model)
-                # docs = db3.similarity_search(last_user_content)
-                # log.info(f"docs >>>  {docs}")
 
 
             r = requests.request(
