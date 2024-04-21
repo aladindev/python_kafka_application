a9bb44e31ac081ce8edeab7d3193f150c1760b66
aladindev <kjune0205@naver.com>
2024-04-21 18:18:58 +0900

    Python Kafka Produce Scheduler

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 
 subprocess.run(["python3", "apps/upbit/scheduler/routers/api_request_upbit.py"])
-result = subprocess.run(["python3", "apps/upbit/scheduler/api_request_scheduler.py"])
+print("start")
+result = subprocess.run(["python3", "apps/upbit/scheduler/api_request_scheduler.py"], capture_output=True, text=True)
 print(result.stdout)
 print(result.stderr)
