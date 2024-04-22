9fbd081850051d63825eeb1ac921ec4b9d6c6ac5
aladindev <kjune0205@naver.com>
2024-04-22 21:11:15 +0900

    Python Kafka Produce Scheduler

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
 
-
-subprocess.run(["python3", "apps/upbit/scheduler/api_request_scheduler.py"])
+#subprocess.run(["python3", "apps/upbit/scheduler/api_request_scheduler.py"])
 #subprocess.run(["python3", "apps/upbit/scheduler/routers/api_request_upbit.py"])
+
+#main 실행이 아닌 스케줄러 직접 실행으로 변경
 
