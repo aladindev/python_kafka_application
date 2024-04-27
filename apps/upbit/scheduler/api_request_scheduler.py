dd8b2088c37c42e7222d81ac448f89772972dfa7
aladindev <kjune0205@naver.com>
2024-04-27 17:32:55 +0900

    Python Kafka Producer Applcation

--- apps/upbit/scheduler/api_request_scheduler.py
+++ apps/upbit/scheduler/api_request_scheduler.py
@@ -8,7 +8,7 @@ import json
 from confluent_kafka import Producer
 
 # 환경변수에서 카프카 서버 주소 가져오기
-kafka_server = os.getenv('KAFKA_SERVER_ADDRESS', 'default_server_address')
+kafka_server = os.getenv('KAFKA_SERVER_ADDRESS', 'default_server_address') #server  -> docker 
 # Kafka producer configuration 환경변수 전달 
 conf = {'bootstrap.servers': kafka_server}
 producer = Producer(**conf)
